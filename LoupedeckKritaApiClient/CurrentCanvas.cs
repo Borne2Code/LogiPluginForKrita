a57675e32e94e3c7da64b360c2b63195d803bce2
Christophe <christophe.borne@gmail.com>
2025-01-17 01:22:06 +0100

    FEAT: complete canvas proxy

--- LoupedeckKritaApiClient/CurrentCanvas.cs
+++ LoupedeckKritaApiClient/CurrentCanvas.cs
@@ -6,8 +6,12 @@ namespace LoupedeckKritaApiClient
     {
         public async Task<float> GetRotation() => await GetFloat("rotation");
         public async Task SetRotation(float rotation) => await Execute("setRotation", rotation);
+        public async Task ResetRotation() => await Execute("resetRotation");
 
         public async Task<float> GetZoomLevel() => await GetFloat("zoomLevel");
         public async Task SetZoomLevel(float zoomLevel) => await Execute("setZoomLevel", zoomLevel);
+        public async Task ResetZoom() => await Execute("resetZoom");
+
+        // TODO: manage bools
     }
 }
