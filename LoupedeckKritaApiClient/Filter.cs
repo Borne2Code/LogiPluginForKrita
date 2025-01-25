c8b409c0b4af760a9d267099333a48b9ad2a8874
Christophe <christophe.borne@gmail.com>
2025-01-25 17:45:27 +0100

    FIX: forgotten file

--- LoupedeckKritaApiClient/Filter.cs
+++ LoupedeckKritaApiClient/Filter.cs
@@ -13,6 +13,13 @@ namespace LoupedeckKritaApiClient
                 FiltersEnum.AscCdl => (FilterDialog)new KritaFilterAscCdl(client),
                 FiltersEnum.AutoConstrast => new KritaFilterAutoConstrast(client),
                 FiltersEnum.Burn => new KritaFilterBurn(client),
+                FiltersEnum.Blur => new KritaFilterBlur(client),
+                FiltersEnum.ColorBalance => new KritaFilterColorBalance(client),
+                FiltersEnum.ColorToAlpha => new KritaFilterColorToAlpha(client),
+                FiltersEnum.ColorTransfer => new KritaFilterColorTranfer(client),
+                FiltersEnum.CrossChannel => new KritaFilterCrossChannel(client),
+                FiltersEnum.Desaturate => new KritaFilterDesaturate(client),
+                FiltersEnum.Dodge => new KritaFilterDodge(client),
                 _ => throw new Exception("Not implement filter dialog")
             };
 
