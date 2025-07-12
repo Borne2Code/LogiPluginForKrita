acf3265e463211fb6f23c846730ae23b8017815a
Christophe <christophe.borne@gmail.com>
2025-07-12 17:24:25 +0200

    FEAT: Add layer group dynamic folder

---
+++ KritaPlugin/DynamicFolders/Layers/LayerGroupsToolsDynamicFolder.cs
@@ -0,0 +1,12 @@
+﻿using Logi.KritaPlugin.Constants;
+
+namespace Logi.KritaPlugin.DynamicFolders
+{
+    public class LayerGroupsToolsDynamicFolder : ToolsDynamicFolderBase
+    {
+        public LayerGroupsToolsDynamicFolder()
+            :base("Groups Tools", ActionGroups.Layers, LayerGroupsToolsConstants.Tools, "Logi.KritaPlugin.images.Layers.NewGroup.png")
+        {
+        }
+    }
+}
